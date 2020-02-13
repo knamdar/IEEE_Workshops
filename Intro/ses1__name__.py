e05a6f8d7bc985b8c5dc46471f8992a6dcc4f7b1
Khashayar Namdar <kh.namdar@hotmail.com>
2020-02-13 15:53:47 -0500

    Add files via upload

---
+++ Intro/ses1__name__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 25 09:10:51 2019
+
+@author: Khashayar
+"""
+import ses1name
+
+ses1name
+#print(ses1name.__name__)
---
+++ Intro/ses1name.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 13 15:18:44 2020
+
+@author: Khashayar
+"""
+
+print("outside if __name__")
+print(__name__)
+if __name__ == "__main__":
+    print("inside if __name__")
+    print(__name__)
